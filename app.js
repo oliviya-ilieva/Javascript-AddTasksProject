6af01712ec3c9829137d9f48860307b4758f0570
Oliviya <oli_31@abv.bg>
2022-06-13 21:13:43 +0300

    filter tasks

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ function loadEventListeners() {
     taskList.addEventListener('click', removeTask);
 
     clearBtn.addEventListener('click', clearTasks);
+
+    filter.addEventListener('keyup', filterTasks);
 }
 
 function addTask(e){
@@ -52,5 +54,19 @@ function clearTasks() {
     taskList.removeChild(taskList.firstCild);
    }
 
+}
+
+
+function filterTasks(e) {
+const text = e.target.value.toLowerCase();
+
+document.querySelectorAll('.collection-item').forEach(function(task){
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1) {
+        task.style.display = 'block';
+    } else {
+        task.style.display = 'none';
+    }
+});
 }
 
